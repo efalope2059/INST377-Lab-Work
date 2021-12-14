e01241e94aa180ded86d2798e0f6f528aa7df6f9
Elijah Falope <efalope@umd.edu>
2021-12-13 19:16:14 -0500

    array comes back by zipcode

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -6,6 +6,15 @@ fetch(endpoint)
   .then((blob) => blob.json())
   .then((data) => cities.push(...data));
 
-function findMatches(wordToMatch, cities){
-    return cities.fitler()
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    // compare search to what is in array //
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.zip.match(regex)
+  });
 }
+
+function displayMatches() {
+    console.log(this.value)
+}
+const searchInput = document.querySelector('search')
