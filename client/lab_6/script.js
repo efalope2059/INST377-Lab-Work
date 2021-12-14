03f6d84703b47b5fff67b5a575a7aacc859f4eeb
Elijah Falope <efalope@umd.edu>
2021-12-13 22:33:03 -0500

    update to standards + styling

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,25 +1,26 @@
 async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-  const cities = [];
+  const request = await fetch(endpoint);
 
-  fetch(endpoint)
-    .then((blob) => blob.json())
-    .then((data) => cities.push(...data));
+  const arrayName = await request.json();
 
-  function findMatches(wordToMatch, cities) {
-    return cities.filter((place) => {
+  function findMatches(wordToMatch, arrayName) {
+    return arrayName.filter((place) => {
     // compare search to what is in array //
       const regex = new RegExp(wordToMatch, 'gi');
       return place.zip.match(regex);
     });
   }
 
-  function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, arrayName);
     const html = matchArray.map((place) => `
-        <li>
-            <span class = "zip-addy"> ${place.zip} : ${place.name}</span>
+        <li class = "results">
+            <span class = "zip-addy"> ${place.name}</span>
+            <span class = "zip-addy"> ${place.category}</span>
+            <span class = "zip-addy"> ${place.address_line_1}</span>
+            <span class = "zip-addy"> ${place.zip}</span>
         </li>
         `).join('');
     suggestions.innerHTML = html;
@@ -30,7 +31,7 @@ async function windowActions() {
   // change event only fires when you click outisde of the form box - not enter //
   // add key up //
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 }
 
 window.onload = windowActions;
