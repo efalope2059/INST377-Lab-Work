ba89b2134720004c72b0f9351d49f10fc11ca893
Elijah Falope <efalope@umd.edu>
2021-12-13 19:58:38 -0500

    Match array and display on console

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -10,11 +10,18 @@ function findMatches(wordToMatch, cities) {
   return cities.filter((place) => {
     // compare search to what is in array //
     const regex = new RegExp(wordToMatch, 'gi');
-    return place.zip.match(regex)
+    return place.zip.match(regex);
   });
 }
 
 function displayMatches() {
-    console.log(this.value)
+  const matchArray = findMatches(this.value, cities);
+  console.log(matchArray);
 }
-const searchInput = document.querySelector('search')
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+// change event only fires when you click outisde of the form box - not enter //
+// add key up //
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
